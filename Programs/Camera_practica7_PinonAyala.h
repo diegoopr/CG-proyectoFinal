15e4c5052015830391651841b01a2807b511afcb
Rodrigopinon12 <31057714+Rodrigopinon12@users.noreply.github.com>
2021-07-22 00:27:58 -0500

    cambio para 3 persona

--- Programs/Camera_practica7_PinonAyala.h
+++ Programs/Camera_practica7_PinonAyala.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <GL/glew.h>
+#include "Window.h"
 
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
@@ -13,7 +14,7 @@ public:
     Camera();
     Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed);
 
-    void keyControl(bool* keys, GLfloat deltaTime);
+    void keyControl(bool* keys, GLfloat deltaTime,GLboolean terceraPersona);
     void mouseControl(GLfloat xChange, GLfloat yChange);
 
     glm::vec3 getCameraPosition();
